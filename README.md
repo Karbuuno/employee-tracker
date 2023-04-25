a21136f572033cc833d07053054f99ca2a2082d8
Abdishakur Abdalla Gesey <karbuuno@gmail.com>
2023-04-25 22:01:23 +0100

    Adde picturs and more in database

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 CLI Content Management System to manage a company's employees. The application links to an SQL database and allows users to view and manage employee records from the command line interface in a more user friendly environment. <br>
 
-Click the [video](https://drive.google.com/file/d/1RuKqt9hB6vpvUZEogbzEEPRJBd0zZhWk/view)
+Click the [video](https://drive.google.com/file/d/171W1tB7m8xu6kj1xQKOLRsrJdr6UK_9_/view)
 
 ![alt image-1](/assets/images/image-1.png)
 ![alt image-1](/assets/images/image-2.png)
--- db/seed.sql
+++ db/seed.sql
@@ -1,43 +1,45 @@
 INSERT INTO department (department_name)
-VALUES ('Sales'),
-('HR'),
+VALUES ('HR'),
 ('Programming'),
-('IT'),
-('Executive');
+('Sales'),
+('IT');
+
 
 INSERT INTO role (title, salary, department_id)
-VALUES('Account Executive', 100000, 1),
-('Sr. Account Executive', 150000, 1),
-('Sales Director', 200000, 1),
-('HR Coordinator', 75000, 2),
-('HR Generalist', 85000, 2),
-('HR Director', 100000, 2),
-('Jr. Developer', 85000, 3),
-('Sr. Developer', 125000, 3),
-('Programming Director', 225000, 3),
-('IT Project Manager', 850000, 4),
-('IT Project Director', 100000, 4),
-('Chief Executive Officer', 300000, 5),
-('Chief Operating Officer', 275000, 5),
-('Chief Financial Officer', 275000, 5);
+VALUES
+
+-- ('Account Executive', 100000, 1),
+-- ('Sr. Account Executive', 150000, 1),
+('Sales Director', 15000, 3),
+('HR Coordinator', 95000, 1),
+-- ('HR Generalist', 85000, 2),
+('HR Director', 110000, 1),
+('Jr. Developer', 100000, 4),
+('Sr. Developer', 140000, 4),
+('Programming Director', 115000, 4),
+('IT Project Manager', 950000, 4);
+-- ('IT Project Director', 100000, 4),
+-- ('Chief Executive Officer', 300000, 5),
+-- ('Chief Operating Officer', 275000, 5),
+-- ('Chief Financial Officer', 275000, 5);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('John', 'Doe', 12, NULL),
-('Abdi', 'Ahmed', 13, 1),
-('Leila', 'Salad', 14, 1),
-('William', 'Robson', 3, 2),
-('Carl', 'Louie', 9, 2),
-('Carl', 'John', 11, 2),
-('Jackie', 'Jackson', 6, 2),
-('Frank', 'Johnson', 1, 4),
-('leila', 'Dodson', 1, 4),
-('Sara', 'john', 2, 4),
-('Frankie', 'Dodson', 4, 7),
-('Bill', 'Frank', 5, 7),
-('Dom', 'Chewer', 5, 7),
-('Mary', 'Bronson', 7, 5),
-('Sarah', 'Robbie', 8, 5),
-('David', 'Johnson', 10, 6),
-('Abdalla', 'Nasser', 10, 6);
+('John', 'Doe', 7, NULL),
+('Abdi', 'Ahmed', 4, 1),
+('Leila', 'Salad', 2, 1),
+('William', 'Robson', 3, NULL),
+('Carl', 'Louie', 6, 1),
+('Carl', 'John', 5, 1),
+('Jackie', 'Jackson', 6, 2);
+-- ('Frank', 'Johnson', 1, 4),
+-- ('leila', 'Dodson', 1, 4),
+-- ('Sara', 'john', 2, 4),
+-- ('Frankie', 'Dodson', 4, 7),
+-- ('Bill', 'Frank', 5, 7),
+-- ('Dom', 'Chewer', 5, 7),
+-- ('Mary', 'Bronson', 7, 5),
+-- ('Sarah', 'Robbie', 8, 5),
+-- ('David', 'Johnson', 10, 6),
+-- ('Abdalla', 'Nasser', 10, 6);
